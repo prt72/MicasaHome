50680b1886c61f6a3d15b9b035610386fa4732ab
Preet <preettt72@gmail.com>
2026-03-08 03:16:46 +0800

    Updated

--- app/Home/page.js
+++ app/Home/page.js
@@ -1,16 +1,16 @@
-// "use client";
+"use client";
 
-// import Link from "next/link";
-// import { useEffect, useRef, useState } from "react";
+import Link from "next/link";
+import { useEffect, useRef, useState } from "react";
 
-// export default function WhyChooseUs({ reasons }) {
-//   // If reasons isn't passed as prop, provide default
-//   const defaultReasons = [
-//     "Post Hospitalisation care / Penjagaan Selepas Hospital",
-//     "Family is at work or overseas / Keluarga Bekerja atau Di Luar Negara",
-//     "Fully Furnished room / Bilik Perabot Lengkap",
-//     "Chronic care assistance / Bantuan Penjagaan Kronik",
-//   ];
+export default function WhyChooseUs({ reasons }) {
+  // If reasons isn't passed as prop, provide default
+  const defaultReasons = [
+    "Post Hospitalisation care / Penjagaan Selepas Hospital",
+    "Family is at work or overseas / Keluarga Bekerja atau Di Luar Negara",
+    "Fully Furnished room / Bilik Perabot Lengkap",
+    "Chronic care assistance / Bantuan Penjagaan Kronik",
+  ];
 
-//   const reasonsList = reasons || defaultReasons;
-// }
+  const reasonsList = reasons || defaultReasons;
+}
